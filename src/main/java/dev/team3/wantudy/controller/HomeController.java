5f2edac3c6ad284f31498f3b8e0db70d2d8947a5
Seok Yun Kim <blacswansong@naver.com>
2021-05-21 17:31:19 +0900

    newLogin

--- src/main/java/dev/team3/wantudy/controller/HomeController.java
+++ src/main/java/dev/team3/wantudy/controller/HomeController.java
@@ -36,18 +36,4 @@ public class HomeController {
 		return "home";
 	}
 
-	@RequestMapping(value = "login", method = RequestMethod.GET)
-	public String login(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);
-
-		return "login";
-	}
-
 }
