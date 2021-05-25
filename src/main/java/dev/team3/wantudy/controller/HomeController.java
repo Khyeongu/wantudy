5d4ddfd5fd1c64cd5b804a194e4e5af77a640d0a
gawibawibo <blacswansong@naver.com>
2021-05-25 14:32:03 +0900

    Update HomeController.java

--- src/main/java/dev/team3/wantudy/controller/HomeController.java
+++ src/main/java/dev/team3/wantudy/controller/HomeController.java
@@ -26,6 +26,7 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 
+		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 
