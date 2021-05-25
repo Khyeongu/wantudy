c2bc18551ba7cc586f32d95d40c935b7031bf9d2
Khyeongu <57424923+Khyeongu@users.noreply.github.com>
2021-05-25 14:20:51 +0900

    Update MemberDAO.java

--- src/main/java/dev/team3/wantudy/dao/MemberDAO.java
+++ src/main/java/dev/team3/wantudy/dao/MemberDAO.java
@@ -7,7 +7,7 @@ import dev.team3.wantudy.dto.MemberDTO;
 public interface MemberDAO {
 
 	void signup(MemberDTO memberDTO) throws SQLException;
-
+	
 	MemberDTO getUser(MemberDTO memberDTO) throws SQLException;
 
 }
