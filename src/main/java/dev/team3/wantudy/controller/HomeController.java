356d6e36c2f064c8e432f0efb971b048737fdc73
Yulme <ymyu123@ajou.ac.kr>
2021-05-26 22:50:56 +0900

    chatwebsocket

--- src/main/java/dev/team3/wantudy/controller/HomeController.java
+++ src/main/java/dev/team3/wantudy/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,4 +37,9 @@ public class HomeController {
 		return "sign/sign";
 	}
 	
+	
+	@GetMapping(value="/chatting")
+	public String chatting() {
+		return "chat";
+	}
 }
---
+++ src/main/java/dev/team3/wantudy/handler/SocketHandler.java
@@ -0,0 +1,42 @@
+package dev.team3.wantudy.handler;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class SocketHandler extends TextWebSocketHandler {
+
+	HashMap<String, WebSocketSession> sessionMap = new HashMap<>();
+
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message) {
+		String msg = message.getPayload();
+		for(String key : sessionMap.keySet()) {
+			WebSocketSession wss = sessionMap.get(key);
+			try {
+				wss.sendMessage(new TextMessage(msg));
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		//家南 楷搬
+		super.afterConnectionEstablished(session);
+		sessionMap.put(session.getId(), session);
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		//家南 辆丰
+		sessionMap.remove(session.getId());
+		super.afterConnectionClosed(session, status);
+	}
+}
