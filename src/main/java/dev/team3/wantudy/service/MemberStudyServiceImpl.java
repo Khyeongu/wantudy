45a42a76b86490e5b3b51fe52dc399579452a2ff
Yulme <ymyu123@ajou.ac.kr>
2021-05-31 11:58:43 +0900

    chatting

--- src/main/java/dev/team3/wantudy/service/MemberStudyServiceImpl.java
+++ src/main/java/dev/team3/wantudy/service/MemberStudyServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 import dev.team3.wantudy.dao.ChattinglogDAO;
 import dev.team3.wantudy.dao.EnrollDAO;
